c9f228ae107268667d08c9a3307c91b03f037f84
kamibog <kameneckaya@mail.ru>
2024-08-08 15:26:43 +0700

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,6 @@
+number_of_completed_tasks = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_per_task = (number_of_hours_spent / number_of_completed_tasks)
+print('Курс'':', course_name, ',''всего задач'':', number_of_completed_tasks, ',' ' затрачено часов'':',
+      number_of_hours_spent, ',' 'среднее время выполнения'':', time_per_task)
